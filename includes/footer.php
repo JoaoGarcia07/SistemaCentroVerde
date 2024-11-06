ffc06d80961a3e4689beb30fe9d3d7c54723f512
Joao Vitor Prestes Garcia <joaovitorprestesgarcia@gmail.com>
2024-11-06 20:29:21 -0300

    Adicionando Base Site

---
+++ includes/footer.php
@@ -0,0 +1,8 @@
+<footer class="container-footer">
+    <div>
+        <p>Copyright &copy;2024 | Desenvolvido por: <a href="https://github.com/ArthurVieiraaa">Arthur</a>, <a href="https://github.com/Felipe-Vieira15">Felipe V</a>, <a href="https://github.com/HenriqueGaldino">Henrique</a>, <a href="https://github.com/Jonathan02M">Jonathan M</a>, <a href="https://github.com/JhonathannHenrique">Jhonathan G</a>.</p>
+    </div>
+</footer>
+<script src="assets/js/script.js"></script>
+</body>
+</html>
---
+++ includes/navbar.php
@@ -0,0 +1,15 @@
+<div class="navbar">
+    <div class="navbar-logo">
+        <img src="" alt="">
+    </div>
+    <div class="links-navbar">
+        <ul>
+            <li>
+                <a href="index.php">Home</a>
+                <a href="parque.php">Parques</a>
+                <a href="sobre.php">Sobre</a>
+                <a href="sugestao.php">Sugestão</a>
+            </li>
+        </ul>
+    </div>
+</div>

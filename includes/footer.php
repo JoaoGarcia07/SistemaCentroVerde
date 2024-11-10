0720e88041ba9cb0759289891d3ddc56d3b416bf
Silas Tristoni <silas.tristoni12@gmail.com>
2024-11-10 18:57:02 -0300

    Iniciando footer

--- includes/footer.php
+++ includes/footer.php
@@ -1,6 +1,20 @@
 <footer class="container-footer">
-    <div>
-        <p>Copyright &copy;2024 | Desenvolvido por: <a href="https://github.com/ArthurVieiraaa">Arthur</a>, <a href="https://github.com/Felipe-Vieira15">Felipe V</a>, <a href="https://github.com/HenriqueGaldino">Henrique</a>, <a href="https://github.com/Jonathan02M">Jonathan M</a>, <a href="https://github.com/JhonathannHenrique">Jhonathan G</a>.</p>
+    <div class="footer-logo">
+        <img src="assets/img/logoCentroVerdeBranco.png">
+    </div>
+    <hr>
+    <div class="footer-content">
+        <div>
+            <div class="nations">
+                <img src="assets/img/bandeiraBrasil.png">
+            </div>
+        </div>
+        <div>
+
+        </div>
+        <div class="links-rede">
+
+        </div>
     </div>
 </footer>
 <script src="assets/js/script.js"></script>
--- index.php
+++ index.php
@@ -1,13 +1,9 @@
-<!DOCTYPE html>
-<html lang="pt-br">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Centro Verde</title>
-    <link rel="stylesheet" href="assets/css/styles.css">
-</head>
-<body>
-    <!-- Navbar -->
+<?php
+$title = "HOME";
+include "includes/header.php"
+?>
+<!-- Navbar -->
+
     <header class="navbar">
         <div class="logo">Centro Verde</div>
         <nav>
@@ -23,17 +19,16 @@
 
     <!-- Section "Sobre Nós" -->
     <section class="about-section">
-        <div class="about-content">
-            <img src="https://via.placeholder.com/150" alt="Grupo de Pessoas" class="about-image">
-            <div class="about-text">
-                <h2>Sobre Nós</h2>
-                <p>Lorem Ipsum é um texto padrão em latim usado em design gráfico e editoração. Ele serve para preencher espaços de texto em layouts, permitindo aos designers focar na aparência visual sem serem distraídos pelo conteúdo. Sua origem remonta a um texto de Cícero, datado de 45 a.C.</p>
+        <div class="about-container">
+            <div class="about-content">
+                <img src="https://via.placeholder.com/150" alt="Grupo de Pessoas" class="about-image">
+                <div class="about-text">
+                    <h2>Sobre Nós</h2>
+                    <p>Lorem Ipsum é um texto padrão em latim usado em design gráfico e editoração. Ele serve para preencher espaços de texto em layouts, permitindo aos designers focar na aparência visual sem serem distraídos pelo conteúdo. Sua origem remonta a um texto de Cícero, datado de 45 a.C.</p>
+                </div>
             </div>
-        </div>
-    </section>
-    
-
-    <!-- Footer -->
-        </div>
-</body>
-</html>
+        </div>       
+</section>
+<?php
+    include "includes/footer.php";
+?>

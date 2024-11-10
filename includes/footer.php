8bbc6dd47a721a1560d6dc421e7e1c79c4a7842f
Silas Tristoni <silas.tristoni12@gmail.com>
2024-11-10 20:23:30 -0300

    Footer completo

--- includes/footer.php
+++ includes/footer.php
@@ -7,14 +7,29 @@
         <div>
             <div class="nations">
                 <img src="assets/img/bandeiraBrasil.png">
+                <h1>Brazil - (Brasil)</h1>
             </div>
+            <a href="#">Política de Privacidade</a>
+            <a href="#">Direitos Autorais</a>
         </div>
         <div>
-
+            <a href="#">Termos de uso</a>
+            <a href="#">Segurança e confiança</a>
+            <a href="#">Política de Acessibilidade</a>
         </div>
         <div class="links-rede">
-
+            <h1>Siga-nos nas redes sociais</h1>
+            <div class="rede-logo">
+                <a href="#" id="instagram"><i class="fa-brands fa-instagram" ></i></a>
+                <a href="#" id="whatsapp"><i class="fa-brands fa-whatsapp"></i></a>
+                <a href="#" id="facebook"><i class="fa-brands fa-facebook"></i></a>
+                <a href="#" id="twitter"><i class="fa-brands fa-twitter"></i></a>
+            </div>
         </div>
+    </div>
+    <hr>
+    <div class="footer-direitos">
+            <h1>@2024 Centro Verde - Todos os direitos reservados</h1>
     </div>
 </footer>
 <script src="assets/js/script.js"></script>

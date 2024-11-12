e5e1a965fc1f94fb6b20ec9779263b805f8a8a22
Joao Vitor Prestes Garcia <joaovitorprestesgarcia@gmail.com>
2024-11-12 19:54:12 -0300

    Arrumando Erros Das Branch

--- index.php
+++ index.php
@@ -1,48 +1,40 @@
 <?php
 $title = "HOME";
-include_once "header.php";
+include "includes/header.php";
 ?>
 
-<div class="main">
-    <div class="home">
-        <div class="home-content">
-            <div class="home-text">
-                <h1>Centro Verde</h1>
-                <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic .</p>
-                <button>Ver Mais</button>
+<?php
+include "includes/navbar.php";
+?>
+
+<section class="home">
+    <div class="home-content">
+        <div class="home-text">
+            <div class="text-content">
+                <h1>Centro <span style="color: #278844;">Verde</span></h1>
+                <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic .</p>
+                <button>Ver mais</button>
             </div>
-            <div class="home-img">
-                <img src="" alt="">
+        </div>
+        <div class="home-img">
+            <div class="img-content">
+                <img src="assets/img/imagemInicial.png">
             </div>
         </div>
     </div>
-    <hr>
-    <div class="home-parques">
-        <div class="home-parques-content">
-            <h2>Busca por Parques populares</h2>
-            <?php
-            include_once "parque.php";
-            ?>
-        </div>
-    </div>
-    <hr>
-    <div class="home-sobre">
-        <div class="home-sobre-content">
-            <?php
-            include_once "sobre.php";
-            ?>
-        </div>
-    </div>
-    <hr>
-    <div class="home-sugestao">
-        <div class="home-sugestao-content">
-            <h2>Sugestões</h2>
-            <?php
-            include "includes/sugestao.php";
-            ?>
-        </div>
-    </div>
-</div>
+</section>
+
+<?php
+include "includes/sobre.php";
+?>
+
+<?php
+include "includes/parques.php";
+?>
+
+<?php
+include "includes/sugestao.php";
+?>
 
 <?php
 include "includes/footer.php";
